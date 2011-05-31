914452cd3d19336abf6d666ae67b38fbf949a5b8
Eric Moritz <emoritz@grabnetworks.com>
2011-05-31 15:21:23 -0400

    Added event log style docs to the Riak store

--- recommendations/load_data.py
+++ recommendations/load_data.py
@@ -7,14 +7,35 @@ import glob
 critics = json.load(open("./mapred_js/critics.json"))
 client = riak.RiakClient(transport_class=riak.RiakPbcTransport, port=8087)
 bucket = client.bucket("critics")
+event_bucket = client.bucket("critics.events")
 
 
 # install the critics data
-for key, value in critics.items():
-    obj = bucket.get(key.encode("utf-8"))
+for name, value in critics.items():
+    obj = bucket.get(name.encode("utf-8"))
     obj.set_data(value)
     obj.set_content_type("application/json")
     obj.store()
+
+    # Store the prefs as events
+    for title, rating in value.items():
+        event_doc = {
+            "timestamp": "1979-01-01T00:00:00",
+            "actor": name,
+            "target": title,
+            "verb": "rate",
+            "score": rating
+            }
+
+        event_key = ";".join([event_doc['timestamp'],
+                              event_doc['actor'],
+                              event_doc['verb'],
+                              event_doc['target']])
+
+        event_obj = event_bucket.get(event_key.encode("utf-8"))
+        event_obj.set_content_type("application/json")
+        event_obj.set_data(event_doc)
+        event_obj.store()
 
 # install the mapred queries
 js_bucket = client.bucket("pci-mapred")
